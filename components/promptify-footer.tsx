7d52f64f04a43a8d95337b7bc3ad3f39c1e4aaae
Tabhi109 <tabhinav109@gmail.com>
2023-08-18 01:16:31 +0530

    created footer component
    Signed-off-by: Tabhi109 <tabhinav109@gmail.com>

--- components/promptify-footer.tsx
+++ components/promptify-footer.tsx
@@ -1,76 +1,34 @@
-// components/Footer.tsx
-
 import React from 'react';
-
+import {FaInstagram,FaLinkedin,FaGithub} from 'react-icons/fa'
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white py-8 text-center">
       <div className="flex justify-center mb-4">
         <div className="mr-4">
           <a
-            href="https://www.linkedin.com/in/your-linkedin-profile"
+            href="https://www.linkedin.com/in/abhinav-tripathi-46872b201/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 21v-6M4 10.5a4 4 0 0 1 4-4h12"
-              ></path>
-            </svg>
+            <FaLinkedin/>
           </a>
         </div>
         <div className="mr-4">
           <a
-            href="https://www.instagram.com/your-instagram-profile"
+            href="https://www.instagram.com/abhinav.trip_/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 8h16M4 16h16"
-              ></path>
-              <circle cx="12" cy="12" r="3"></circle>
-            </svg>
+            <FaInstagram/>
           </a>
         </div>
         <div>
           <a
-            href="https://github.com/your-github-profile"
+            href="https://github.com/Tabhi109"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 19c-5 1.5-5-2.5-7-3m14 6.5l-2.35-2.35A7.82 7.82 0 0 0 12 17a7.82 7.82 0 0 0-2.65 3.15L7 25.5M16.65 15.15a7.82 7.82 0 0 0 0-10.3M22 12h-2M4 12H2"
-              ></path>
-            </svg>
+            <FaGithub/>
           </a>
         </div>
       </div>
