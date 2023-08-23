457316320d44aca3c821b2e498126352dcbffe25
Tabhi109 <tabhinav109@gmail.com>
2023-08-23 23:14:10 +0530

    updated API for encrypting the password
    Signed-off-by: Tabhi109 <tabhinav109@gmail.com>

--- pages/api/user/create.ts
+++ pages/api/user/create.ts
@@ -1,13 +1,24 @@
-import {NextApiResponse,NextApiRequest} from 'next';
+import { NextApiResponse, NextApiRequest } from 'next';
 import { sql } from '@vercel/postgres';
+import { createHash } from 'crypto';
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    const data = req.body;
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const data = req.body;
+
+  // Hash the password using SHA-256 algorithm
+  const hashedPassword = createHash('sha256').update(data.password).digest('hex');
+
+  try {
     const { rows } = await sql`
-    insert into users 
-    (first_name,last_name,profile_picture,email,phone,bio,date_of_birth,location,city,preferred_tags)
-    values 
-    (${data.first_name},${data.last_name},${data.profile_picture},${data.email},${data.phone},${data.bio},${data.date_of_birth},${data.location},${data.city},${data.preferred_tags})
-    returning id`;
-    res.status(200).json({ rows })
+      insert into users 
+      (first_name,last_name,profile_picture,email,phone,bio,date_of_birth,location,city,preferred_tags,password)
+      values 
+      (${data.first_name},${data.last_name},${data.profile_picture},${data.email},${data.phone},${data.bio},${data.date_of_birth},${data.location},${data.city},${data.preferred_tags},${hashedPassword})
+      returning id`;
+
+    res.status(200).json({ rows });
+  } catch (error) {
+    console.error('Error inserting user:', error);
+    res.status(500).json({ error: 'An error occurred while inserting user.' });
   }
+}
